e1114cf10743bbc14fce692e9bfbca66b3750b0b
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-03 00:04:43 -0500

    class Dog and added a getter

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dog
+{
+    class Dog
+    {
+       // "If lost, call [owner]. ['Her'|'His'] name is [
+        public String name = "";
+        public int age; 
+        public String owner = "";
+
+        Dog(String owner, String name, int age, Gender g){ //Constructor
+            this.owner = owner;
+            this.name = name;
+            this.age = age; 
+            // this,
+
+        }
+        int bark(int num_barks){
+            for (int i = 0; i < num_barks; i++)
+            {
+                Console.WriteLine("Woof!");
+            }
+
+        }
+
+        String getTag(){
+
+            return "If lost, call {0}. ['Her'|'His'] name is {1} and ['she'|'he'] is {2}  ['year'|'years'] old.", this.owner, this.name, this.age;;  
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
